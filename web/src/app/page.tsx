59d2f9ee9f944e0bd6fd16e09679284390a1c482
daniel carvalho <dscarv27@gmail.com>
2023-05-17 16:45:07 -0300

    primeiras estilizações web - right

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
-  return (  
-    <div className="h-screen bg-zinc-950 text-zinc-50">
-      sua cápsula do tempo
-    </div>
+  return (
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between">
+
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a {' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
+
 }
