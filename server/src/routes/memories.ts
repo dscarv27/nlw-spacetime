fb3b6c91112baa83d415b3f574a5dac054215cf0
daniel carvalho <dscarv27@gmail.com>
2023-05-18 11:52:13 -0300

    iniciando crud

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findFirstOrThrow({
+      where: {
+        id,
+      },
+    })
+    return memory
+  })
+
+  app.post('/memories', async () => {})
+
+  app.put('/memories', async () => {})
+
+  app.delete('/memories', async () => {})
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,9 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-// HTTP METHODS: GET: listar, POST: criar, PUT: atualizar,
-// PATCH: atualizar uma informação específica, DELETE: deletar
-// API RESTFUL: existem padrões a serem seguidos
-
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
