c05aafebb7ddf9a7880e4532d23362b42d8806f1
daniel carvalho <dscarv27@gmail.com>
2023-05-20 14:24:08 -0300

    token jwt e rotas autenticadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderão acessar nosso backend
 })
+
+app.register(jwt, {
+  secret: 'spacetime', // no mundo real, utilizar string totalmente aleatória
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
